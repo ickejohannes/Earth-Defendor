3192d9f1496d5251887f7819144824af733ac8e0
ickejohannes <ickejohannes@gmail.com>
2022-05-16 12:16:43 +0200

    Add asset for player shots, correct spawning pos of player shots

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
+//player laser is from here https://redfoc.com/item/space-shooter-game-assets/
+
+
 const canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 
 const audio = new Audio('./assets/audio/game.mp3');
 
 
-const img = new Image();
-img.src = "./assets/images/bg.jpg";
+const backgroundImage = new Image();
+backgroundImage.src = "./assets/images/bg.jpg";
 
-const ship = new Image();
-ship.src = "./assets/images/rocket-147466_640.png";
+const shipImage = new Image();
+shipImage.src = "./assets/images/rocket-147466_640.png";
 xBaseSpeed = 1;
 yBaseSpeed = -1;
 
 let shipRotation = 0;
 let rotationSpeed = 5;
 
+
+const shotImage = new Image();
+shotImage.src = "./assets/images/shot.png"
 let shots = [];
 
 
 
 window.onload = () => {
     start();
-    ctx.drawImage(img, 0, 0);
-    ctx.drawImage(ship, canvas.width/2, canvas.height/2, 50, 50);
 };
   
 function start() {
-    // new Audio("").play()
-    // intervalID = setInterval(createObstacle, obstacleFrequency);
     updateCanvas();
 }
   
 function updateCanvas() {
-    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(backgroundImage, 0, 0);
    
     for (iterator = 0; iterator < shots.length; iterator += 1) {
-        ctx.fillRect(shots[iterator].xPos, shots[iterator].yPos, 20, 20);
+        ctx.drawImage(shotImage, shots[iterator].xPos, shots[iterator].yPos, 10, 10);
         shots[iterator].yPos -= shots[iterator].ySpeed;
         shots[iterator].xPos += shots[iterator].xSpeed;
     }
@@ -43,7 +45,7 @@ function updateCanvas() {
     ctx.save();
     ctx.translate(700,400);
     ctx.rotate(shipRotation * Math.PI / 180);
-    ctx.drawImage(ship, -25, -25, 50, 50);
+    ctx.drawImage(shipImage, -25, -25, 50, 50);
     ctx.restore();
 
 
@@ -86,7 +88,7 @@ class Shot {
         this.ySpeed = Math.sin(radians) * yBaseSpeed;
         // this.xSpeed = xBaseSpeed * Math.sin(shipRotationAtShot);
         // this.ySpeed = yBaseSpeed * Math.cos(shipRotationAtShot);
-        this.xPos = 700;
-        this.yPos = 400;
+        this.xPos = 695;
+        this.yPos = 395;
     }
 }
