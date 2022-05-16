7d9231cab73f049247a809639b7442272523df7b
ickejohannes <ickejohannes@gmail.com>
2022-05-16 14:09:31 +0200

    Add random enemy movement

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ backgroundImage.src = "./assets/images/bg.jpg";
 
 const shipImage = new Image();
 shipImage.src = "./assets/images/rocket-147466_640.png";
+let playerPosX = 700;
+let playerPosY = 400;   
+
 xBaseSpeed = 1;
 yBaseSpeed = -1;
 let shipRotation = 0;
@@ -38,7 +41,7 @@ window.onload = () => {
   
 function start() {
     updateCanvas();
-    setInterval(createEnemy, enemyFrequency);
+    setInterval(createEnemy, enemyFrequency); 
 }
   
 function updateCanvas() {
@@ -51,12 +54,14 @@ function updateCanvas() {
     }
 
     for (iterator = 0; iterator < enemyArray.length; iterator += 1) {
-        console.log(enemyArray[iterator])
-        ctx.drawImage(enemyImage, enemyArray[iterator].enemyXPos, enemyArray[iterator].enemyYPos, enemyArray[iterator].enemyWidth, enemyArray[iterator].enemyHeight);
+        let enemy = enemyArray[iterator];
+        ctx.drawImage(enemyImage, enemy.enemyXPos, enemy.enemyYPos, enemy.enemyWidth, enemy.enemyHeight);
+        enemy.enemyXPos += enemy.enemyXSpeed;
+        enemy.enemyYPos += enemy.enemyYSpeed;  
     }
 
     ctx.save();
-    ctx.translate(700,400);
+    ctx.translate(playerPosX,playerPosY);
     ctx.rotate(shipRotation * Math.PI / 180);
     ctx.drawImage(shipImage, -25, -25, 50, 50);
     ctx.restore();
@@ -89,18 +94,18 @@ function rotateShipCounterClockwise() {
     shipRotation += rotationSpeed;
 }
 
+
 function shoot() {
     shots.push(new Shot(shipRotation));
     audio.play();
    
 }
 
+
 class Shot {
     constructor(shipRotationAtShot) {
         let radians = shipRotationAtShot * Math.PI / 180
-        
         this.xSpeed = Math.cos(radians) * xBaseSpeed
-        console.log(Math.cos(radians) * xBaseSpeed)
         this.ySpeed = Math.sin(radians) * yBaseSpeed;
         this.xPos = 695;
         this.yPos = 395;
@@ -108,14 +113,58 @@ class Shot {
 }
 
 class Enemy {
-    constructor(xPos, yPos) {
+    constructor(xPos, yPos, xSpeed, ySpeed) {
         this.enemyXPos = xPos;
         this.enemyYPos = yPos;
+        this.enemyHeight = 60;
+        this.enemyWidth = 30;
+        this.enemyXSpeed = xSpeed;
+        this.enemyYSpeed = ySpeed;
     };
-    enemyHeight = 60;
-    enemyWidth = 30;
+    
   }
-  
-  function createEnemy() {
-    enemyArray.push(new Enemy(0,0));
-  }
+
+function createEnemy() {
+    let enemySpawnDirection = chooseRandomSpawnDirection();
+    let enemySpawnPosition = createSpawnPosition(enemySpawnDirection);
+    let enemySpeed = createEnemySpeed(enemySpawnDirection);
+    enemyArray.push(new Enemy(enemySpawnPosition[0], enemySpawnPosition[1], enemySpeed[0], enemySpeed[1]));
+}
+
+function chooseRandomSpawnDirection() {
+    let randomNumber = Math.random()
+    if (randomNumber < 0.25) {
+        return "left";
+    } else if (randomNumber < 0.5) {
+        return "top";
+    } else if (randomNumber < 0.75) {
+        return "right";
+    } else {
+        return "bottom";
+    }
+
+}
+
+function createSpawnPosition(direction) {
+    if (direction == "left") {
+        return [-50, Math.random() * 800];
+    } else if (direction == "top") {
+        return [Math.random() * 1500, -50];
+    } else if (direction == "right") {
+        return [1550, Math.random() * 800];
+    } else {
+        return [Math.random() * 1500, 850];
+    }
+}
+
+function createEnemySpeed(direction) {
+    if (direction == "left") {
+        return [1, Math.random()-0.5];
+    } else if (direction == "top") {
+        return [Math.random()-0.5, 1];
+    } else if (direction == "right") {
+        return [-1, -(Math.random()-0.5)];
+    } else {
+        return [-(Math.random()-0.5) , -1];
+    }
+}
